36806edf20103d7caa9a72811150fba5386239a1
westkite1201 <westkitedev@gmail.com>
2019-11-01 23:11:41 +0900

    style 변경

--- GloryMorning_front/src/components/WeatherInfo/DustInfo/DustInfo.js
+++ GloryMorning_front/src/components/WeatherInfo/DustInfo/DustInfo.js
@@ -69,6 +69,7 @@ const DustInfo = observer(() => {
   let selectDustMessageInfo = weather.selectDustMessageInfo;
   console.log("[SEO] selectDustMessageInfo" ,selectDustMessageInfo)
   console.log("[SEO] dustInfoObject", dustInfoObject);
+  let fontColor = 'white';
   return (
       <div>
       {/*
@@ -130,29 +131,40 @@ const DustInfo = observer(() => {
 
         <div className="dust_info_container">
           <div className="station_info">
-              <div> 측정일 {dustInfoObject.dataTime} </div>
-              <div>
-              {" "}
-              가장 가까운 관측소와의 거리는 {dustInfoObject.distance} 입니다
+              <div  className="station_info_date"> 
+                측정일
+                <div  className="station_info_dateTime"> 
+                  {dustInfoObject.dataTime} 
+                </div> 
               </div>
-              <div> 관측소 이름: {dustInfoObject.addr}</div>
+
+              <div className="station_info_distance">
+              {" "}
+                관측소와의 거리 :  {dustInfoObject.distance}km
+              </div>
+              <div  className="station_info_name"> 
+                관측소 이름 
+              </div>
+              <div  className="station_info_addr">
+                {dustInfoObject.addr} 
+              </div>
           </div>
 
         {!_.isNil(dustInfoObject[selectDustMessageInfo]) &&
           <div className="display_icon_wrapper">
-              <div className="info_header">
+              <div className="info_header" style ={{color : fontColor}}>
                 {dustInfoObject[selectDustMessageInfo].InfoHeader}
               </div>
               <div className="info_icon">
               {dustInfoObject[selectDustMessageInfo].infoIcon}
               </div>
-              <div>
+              <div className= "info_name" >
                 {dustInfoObject[selectDustMessageInfo].name}
               </div>
-              <div className="info_value"> 
+              <div className="info_value" style ={{color : dustInfoObject[selectDustMessageInfo].color}}> 
                 {dustInfoObject[selectDustMessageInfo].value}
               </div>
-              <div className="infoMessage">
+              <div className="info_message" style ={{color : dustInfoObject[selectDustMessageInfo].color}}>
               {dustInfoObject[selectDustMessageInfo].infoMessage}
               </div>
           </div>
