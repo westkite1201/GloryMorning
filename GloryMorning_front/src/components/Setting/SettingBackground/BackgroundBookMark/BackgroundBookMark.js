c6540dadaca5a816369bd7fc73dcc5c1cc0fcc0b
westkite1201 <westkite1201@gmail.com>
2020-02-13 23:48:02 +0900

    setting background  item grid

--- GloryMorning_front/src/components/Setting/SettingBackground/BackgroundBookMark/BackgroundBookMark.js
+++ GloryMorning_front/src/components/Setting/SettingBackground/BackgroundBookMark/BackgroundBookMark.js
@@ -2,20 +2,49 @@ import React, { useEffect } from "react";
 import { observer } from "mobx-react";
 import UseStores from "../../UseStores.js";
 import BackgroundBookMarkItem from "./BackgroundBookMarkItem";
+import Grid from "@material-ui/core/Grid";
 
 const BackgroundBookMark = observer(() => {
   const { setting } = UseStores();
   console.log("[SEO] BackgroundBookMark RENDER ", setting.backgroundBookMark);
+
   const makeList = () => {
-    setting.backgroundBookMark.map((item, key) => {
-      console.log("[seo] item ", item);
+    let arr = [];
+    let tempArr = [];
+
+    setting.backgroundBookMark.map((item, index) => {
+      console.log("[SEOTEST]  index ", index);
+      tempArr.push(Object.assign({}, item));
+      if ((index + 1) % 3 === 0) {
+        arr.push(tempArr);
+        tempArr = [];
+      } else {
+        if (!arr[arr.length - 1]) {
+          arr.push(tempArr);
+        } else {
+          arr[arr.length - 1] = tempArr;
+        }
+      }
+    });
+    console.log("[SEOTEST]  arr ", arr);
+    return arr.map((gridItemList, key) => {
+      let backgroundRow = gridItemList.map((item, key) => {
+        return (
+          <Grid item xs={3}>
+            <BackgroundBookMarkItem
+              key={key}
+              setting={setting}
+              item={item}
+              filterBookMarkBackGround={setting.filterBookMarkBackGround}
+            />
+          </Grid>
+        );
+      });
+
       return (
-        <BackgroundBookMarkItem
-          key={key}
-          setting={setting}
-          item={item}
-          filterBookMarkBackGround={setting.filterBookMarkBackGround}
-        />
+        <Grid container item xs={12} spacing={3}>
+          {backgroundRow}
+        </Grid>
       );
     });
   };
@@ -26,12 +55,23 @@ const BackgroundBookMark = observer(() => {
   return (
     <div>
       <div>PHOTOS</div>
-      {setting.backgroundBookMark.map((item, key) => {
+      <Grid container spacing={1}>
+        <Grid container item xs={12} spacing={3}></Grid>
+        <Grid container item xs={12} spacing={3}></Grid>
+        <Grid container item xs={12} spacing={3}></Grid>
+      </Grid>
+      {/*setting.backgroundBookMark.map((item, key) => {
         console.log("[seo] item ", item);
         return (
-          <BackgroundBookMarkItem key={key} setting={setting} item={item} />
+          <BackgroundBookMarkItem
+            key={key}
+            setting={setting}
+            item={item}
+            filterBookMarkBackGround={setting.filterBookMarkBackGround}
+          />
         );
-      })}
+      })*/}
+      {makeList()}
     </div>
   );
 });
