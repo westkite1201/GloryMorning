1433182134db913999de8a0f8d4c3bdf66aefb9f
westkite1201 <westkite1201@gmail.com>
2020-06-14 14:48:27 +0900

    RainComponent 정리

--- GloryMorning_front/src/components/Rain/Rain.js
+++ GloryMorning_front/src/components/Rain/Rain.js
@@ -10,39 +10,9 @@ let height = 1000;
 export default class Rain extends Component {
   componentDidMount() {
     let defaultYn = true;
-    //init(width, height, true);
     loadTextures(width, height, defaultYn);
   }
 
-  reRenderRain = () => {
-    let awidth = 400;
-    let aheight = 400;
-    let dpi = window.devicePixelRatio;
-    // context
-    var canvas = document.querySelector('#rain-container');
-    var ctx = canvas.getContext('2d');
-    var gl = canvas.getContext('webgl'); // will always be null
-    // Set the viewport
-    // ctx3d.viewport(0, 0, this.width, this.height);
-    // ctx3d.clearColor(0, 0, 0, 0);
-
-    gl.viewport(0, 0, 400, 400);
-    gl = canvas.getContext('webgl', { alpha: false });
-    // gl.colorMask(false, false, false, true);
-    // gl.clearColor(0, 0, 0, 1);
-    // gl.clear(gl.COLOR_BUFFER_BIT);
-    // Clear the canvas.
-    //gl.clear(gl.COLOR_BUFFER_BIT);
-
-    // gl.getExtension('WEBGL_lose_context').restoreContext();
-    canvas.width = awidth * dpi;
-    canvas.height = aheight * dpi;
-    canvas.style.width = awidth + 'px';
-    canvas.style.height = aheight + 'px';
-    //init(awidth, aheight, false)
-    //loadTextures(awidth, aheight, false)
-    this.setState({});
-  };
   render() {
     console.log('render');
     // const script = document.createElement("script");
